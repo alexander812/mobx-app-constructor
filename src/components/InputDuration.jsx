2d6a53053e8307b56455df5bfac1a64d7cf069a0
Alexander Smirnov <a.smirnov@space307.com>
2018-02-11 13:35:35 +0300

    input duration 2

--- src/components/InputDuration.jsx
+++ src/components/InputDuration.jsx
@@ -3,8 +3,35 @@ export function trim(str) {
   return str.replace(new RegExp('^[^\\d\\:]+|[^\\d\\:]+$', 'g'), '');
 }
 export function formatNumber(str, maskParam) {
-  const arr = str.split('');
-  let mask = maskParam;
+  //const arr = str.split('');
+
+  let result = maskParam;
+  const [maskBefore, maskAfter] = maskParam.split(':');
+  const [strBefore, strAfter] = str.split(':');
+  let resultBefore = strBefore && strBefore.split('');
+  let resultAfter = strAfter && strAfter.split('');
+
+
+  maskBefore.split('').forEach((val, index) => {
+    const letter = resultBefore && resultBefore[index];
+    const replacement = /[\d]/.test(letter)
+      ? letter : '';
+    result = result.replace(/x/, replacement);
+  });
+
+
+  maskAfter.split('').forEach((val, index) => {
+    const letter = resultAfter && resultAfter[index];
+    const replacement = /[\d]/.test(letter)
+      ? letter : '';
+    result = result.replace(/x/, replacement);
+  });
+
+
+  return result;
+
+
+/*  let mask = maskParam;
   arr.forEach((letter) => {
     if (/[\d\+]/.test(letter)) {
       mask = mask.replace(/x/, letter);
@@ -13,9 +40,10 @@ export function formatNumber(str, maskParam) {
 
   mask = trim(mask.replace(/x(-)/g, '').replace(/x/g, ''));
 
-  return mask;
+  return mask;*/
 }
 export function setCaret(tearget, caretPos) {
+
   if (tearget != null) {
     if ('selectionStart' in tearget) {
       tearget.selectionStart = caretPos;
@@ -68,24 +96,69 @@ export default class InputDuration extends React.Component {
   onChange(e){
     const t = e.currentTarget;
     let result = t.value;
-
-    result = formatNumber(result, 'xx:xx');
-
     let caret;
     if (e.target instanceof HTMLInputElement) {
       caret = e.target.selectionStart;
     }
-    const oldValue = this.state.value;
-    const newValue = result;
-    const diffClear = newValue.replace(/[^\d]+/g, '').length - oldValue.replace(/[^\d]+/g, '').length;
 
-    const diffReal = newValue.length - oldValue.length;
-    const diff = caret < oldValue.length && diffReal > diffClear ? diffClear : diffReal;
-    const newCaret = caret + (diff >= 0 ? diff - 1 : 0);
-    if (newCaret >= 0) {
-      this.caret = newCaret;
+/*
+    if(!/[\:]/.test(result)){
+      return;
     }
-    this.setState({value: result});
+*/
+
+
+
+    if(this.isValid(result)){
+
+      result = formatNumber(result, 'xx:xx');
+
+
+      const oldValue = this.state.value;
+      const newValue = result;
+      const diffClear = newValue.replace(/[^\d]+/g, '').length - oldValue.replace(/[^\d]+/g, '').length;
+
+      const diffReal = newValue.length - oldValue.length;
+      const diff = caret < oldValue.length && diffReal > diffClear ? diffClear : diffReal;
+      const newCaret = caret + (diff >= 0 ? diff - 1 : 0);
+      if (newCaret >= 0) {
+        this.caret = newCaret;
+      }
+
+      if(result){
+        this.setState({value: result});
+      }
+    } else {
+
+      setTimeout(()=>{
+        setCaret(this.input, this.caret);
+      }, 0);
+
+    }
+
+
+
+
+  }
+
+  isValid(val){
+
+    let result = true;
+    const arr = val.split(':');
+    const [before, after] = arr;
+    const beforeNumber = Number(before);
+    const afterNumber = Number(after);
+
+    if(arr.length < 2){
+      result = false;
+    } else if(before.length && beforeNumber > 23){
+      result = false;
+    } else if(after.length && afterNumber > 59){
+      result = false;
+    }
+
+
+    return result;
   }
 
 
